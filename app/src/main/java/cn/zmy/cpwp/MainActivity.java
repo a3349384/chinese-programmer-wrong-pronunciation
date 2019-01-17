70dbce90a3e16e2a7d196d2854268d7913da4e7a
mingyao.zhou <mingyao.zhou@618tech.com>
2019-01-17 16:25:46 +0800

    新增：主页面增加菜单

--- app/src/main/java/cn/zmy/cpwp/MainActivity.java
+++ app/src/main/java/cn/zmy/cpwp/MainActivity.java
@@ -1,17 +1,52 @@
 package cn.zmy.cpwp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import cn.zmy.cpwp.data.WordsFactoryDirector;
+import cn.zmy.common.utils.IntentUtil;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.wordslist, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menuDataSource:
+            {
+                try
+                {
+                    IntentUtil.openWithBrowser(this, "https://github.com/shimohq/chinese-programmer-wrong-pronunciation");
+                }
+                catch (Exception ignored){}
+                break;
+            }
+            case R.id.menuViewSource:
+            {
+                try
+                {
+                    IntentUtil.openWithBrowser(this, "https://github.com/a3349384/chinese-programmer-wrong-pronunciation");
+                }
+                catch (Exception ignored){}
+                break;
+            }
+        }
+        return true;
     }
 }
